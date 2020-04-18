7b7143dcd6b29720304f22bbceaef7f859a3c1e2
Raghu Sharma <raghusharma0404@gmail.com>
2020-04-19 01:57:22 +0530

    Add files via upload

---
+++ FEND capstone Travel App/README.md
@@ -0,0 +1,27 @@
+# Travel App 
+
+## Project Seven - Travel App (Front End Developer Udacity Nanodegree)
+#### What I used: HTML, CSS, Javascript (Vanilla)
+
+Run Commands:
+To install packages 
+npm install
+
+To build dev
+npm run build-dev
+
+To build prod
+npm run build-prod
+
+To run
+npm start
+
+TO perform jest test
+npm run test
+
+Hope you find this helpful!!!!!!
+
+
+
+
+copyright - Raghu Sharma
---
+++ FEND capstone Travel App/dist/precache-manifest.a03ddd24a0c083dd64be00d7bd09a6c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e195d9e6ccaca0e002deb8ecb73ae6c",
+    "url": "./index.html"
+  },
+  {
+    "url": "8c424b62aa096536c1c8ebd246d33ae6.jpg"
+  },
+  {
+    "url": "be53261e75b4dfda1e799e275f612d2b.png"
+  },
+  {
+    "url": "d8e5ce48522ea362897a9827619203f3.png"
+  },
+  {
+    "url": "e52085ff608e697e4c19c6d8ca4d535e.png"
+  },
+  {
+    "revision": "ac0a04cce43fd8767981",
+    "url": "main.css"
+  },
+  {
+    "revision": "ac0a04cce43fd8767981",
+    "url": "main.js"
+  },
+  {
+    "revision": "7469d63ad1399e0c3c222882b8606507",
+    "url": "main.js.LICENSE"
+  }
+]);
