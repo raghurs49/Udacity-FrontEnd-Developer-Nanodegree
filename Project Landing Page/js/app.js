0a223effe3d0376b3fb7278f38dbea8e45bc9f19
Raghu Sharma <raghusharma0404@gmail.com>
2020-04-19 01:51:17 +0530

    Add files via upload

---
+++ Project Landing Page/js/app.js
@@ -0,0 +1,110 @@
+//Build the menu dynamically
+
+let unorderedList = document.getElementById("unordered-list");
+let containerCount = document.getElementsByClassName("landing__container")
+  .length;
+
+for (i = 1; i < containerCount + 1; i++) {
+  let item = "#section" + i + "-title";
+  let itemValue = document.querySelector(item);
+  let itemText = itemValue.textContent;
+  let newLine = document.createElement("li");
+  let lineText = document.createTextNode(itemText);
+  let listItem = "sample-nav-" + i;
+  newLine.setAttribute("id", listItem);
+  newLine.setAttribute("class", "nav-list-item");
+  newLine.appendChild(lineText);
+  unorderedList.appendChild(newLine);
+
+  let itemTarget = document.getElementById("section" + i);
+  let listTarget = document.getElementById(listItem);
+  let buttonName = "section" + i + "-button";
+  let buttonToAdd = document.getElementById(buttonName);
+
+  //Adds button when scrolls into view
+
+  listTarget.addEventListener("click", function() {
+    itemTarget.scrollIntoView ({
+      behavior: 'smooth'}
+    )
+//Added behavior smooth as per review. There is now a visible scroll
+    buttonToAdd.innerHTML =
+      "<button class='section-button' onclick='goToTop()'>Return to Top</button>";
+  });
+}
+
+
+//Changed from 'document.documentElement.scrollTop = 0' method to a visible scroll to top as per reviewer
+
+
+//This is the helper function for a scroll (I set it a bit slow for visibility)
+const scrollToTop = () => {
+  const scrolling = document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrolling > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, scrolling - scrolling / 50);
+  }
+};
+
+
+// On button click, goes to top of page 
+function goToTop() {
+scrollToTop();
+
+  for (i = 1; i < containerCount + 1; i++) {
+    let buttonToDelete = document.getElementById("section" + i + "-button");
+    buttonToDelete.innerHTML = "";
+  }
+}
+
+//Checks if section is in view and adds active-class with moving background and color change
+function checkIfSectionInView() {
+  let isInViewport = function(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top <= 50 &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  for (i = 1; i < containerCount + 1; i++) {
+    let sectionInFullView = document.getElementById("section" + i);
+
+    window.addEventListener(
+      "scroll",
+      function(event) {
+        if (isInViewport(sectionInFullView)) {
+          sectionInFullView.classList.add("your-active-class");
+        } else {
+          sectionInFullView.classList.remove("your-active-class");
+        }
+      },
+      false
+    );
+  }
+}
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("header");
+
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+checkIfSectionInView();
---
+++ Project Landing Page/readme.md
@@ -0,0 +1,13 @@
+# Landing Page Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open app.js to and run it to better know the dynamics happening in code.
+
+Run app.js and open index.html to find the desired results.
